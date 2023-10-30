97ddf8fb40a43675dfeb0e07ab5ef606a7cabac6
Matyrobbrt <matyrobbrt@gmail.com>
2023-10-30 12:52:38 +0200

    Bump nashorn and enable es6 mode

--- src/main/java/net/neoforged/coremod/NashornFactory.java
+++ src/main/java/net/neoforged/coremod/NashornFactory.java
@@ -9,8 +9,14 @@ import org.openjdk.nashorn.api.scripting.NashornScriptEngineFactory;
 import org.openjdk.nashorn.api.scripting.ScriptObjectMirror;
 
 class NashornFactory {
+    private static final String[] ARGS = new String[] {"--language=es6"};
     static ScriptEngine createEngine() {
-        return new NashornScriptEngineFactory().getScriptEngine(CoreModEngine::checkClass);
+        return new NashornScriptEngineFactory().getScriptEngine(ARGS, getAppClassLoader(), CoreModEngine::checkClass);
+    }
+
+    private static ClassLoader getAppClassLoader() {
+        final ClassLoader ccl = Thread.currentThread().getContextClassLoader();
+        return (ccl == null) ? NashornScriptEngineFactory.class.getClassLoader() : ccl;
     }
 
     @SuppressWarnings("unchecked")
